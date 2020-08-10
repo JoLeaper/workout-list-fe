717c7125ea0d3caba8a4007af481295ce0a84335
JoLeaper <joleap123@gmail.com>
2020-08-10 16:57:24 -0700

    action test written for setWorkout

---
+++ src/actions/workoutActions.test.js
@@ -0,0 +1,22 @@
+describe('workout actions', () => {
+  it('creates a SET_WORKOUTS action', () => {
+    const action = setWorkouts([{
+      name: 'Bench Press',
+      muscles: 'Chest and Tris',
+      description: 'Lift the bar',
+      reps: 25,
+      weight: 155,
+    }]);
+
+    expect(action).toEqual({
+      type: SET_WORKOUTS,
+      payload: [{
+        name: 'Bench Press',
+        muscles: 'Chest and Tris',
+        description: 'Lift the bar',
+        reps: 25,
+        weight: 155,
+      }]
+    });
+  });
+});
---
+++ src/reducers/workoutReducers.test.js
@@ -0,0 +1,12 @@
+describe('characters reducers', () => {
+  it('handles the SET_WORKOUTS action', () => {
+    const state = {
+      workouts: [],
+      newWorkout: {}
+    };
+  });
+
+  it('handles the ADD_NEW_WORKOUT action', () => {
+    
+  });
+});

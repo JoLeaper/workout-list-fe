b1933a5b54691cb053cddf633bc7cbb5dafd99f4
JoLeaper <joleap123@gmail.com>
2020-08-10 17:53:40 -0700

    reducer tests written

--- src/reducers/workoutReducers.test.js
+++ src/reducers/workoutReducers.test.js
@@ -1,12 +1,60 @@
+const { setWorkouts, setNewWorkout } = require('../actions/workoutActions');
+const { default: reducer } = require('./workoutsReducer');
+
 describe('characters reducers', () => {
   it('handles the SET_WORKOUTS action', () => {
     const state = {
       workouts: [],
       newWorkout: {}
     };
+
+    const action = setWorkouts([{
+      name: 'Bench Press',
+      muscles: 'Chest and Tris',
+      description: 'Lift the bar',
+      reps: 25,
+      weight: 155,
+    }]);
+
+    const newState = reducer(state, action);
+
+    expect(newState).toEqual({
+      workouts: [{
+        name: 'Bench Press',
+        muscles: 'Chest and Tris',
+        description: 'Lift the bar',
+        reps: 25,
+        weight: 155,
+      }],
+      newWorkout: {}
+    });
   });
 
-  it('handles the ADD_NEW_WORKOUT action', () => {
-    
+  it('handles the SET_NEW_WORKOUT action', () => {
+    const state = {
+      workouts: [],
+      newWorkout: {}
+    };
+
+    const action = setNewWorkout({
+      name: 'Bench Press',
+      muscles: 'Chest and Tris',
+      description: 'Lift the bar',
+      reps: 25,
+      weight: 155,
+    });
+
+    const newState = reducer(state, action);
+
+    expect(newState).toEqual({
+      workouts: [],
+      newWorkout: {
+        name: 'Bench Press',
+        muscles: 'Chest and Tris',
+        description: 'Lift the bar',
+        reps: 25,
+        weight: 155,
+      }
+    });
   });
 });

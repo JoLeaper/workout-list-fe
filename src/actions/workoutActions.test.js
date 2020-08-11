9cbb22ede6b07e06c2fcf504b95ac5feaa449609
JoLeaper <joleap123@gmail.com>
2020-08-10 17:09:15 -0700

    set new workout test written

--- src/actions/workoutActions.test.js
+++ src/actions/workoutActions.test.js
@@ -21,4 +21,25 @@ describe('workout actions', () => {
       }]
     });
   });
+
+  it('creates a SET_NEW_WORKOUT action', () => {
+    const action = setNewWorkout({
+      name: 'Bench Press',
+      muscles: 'Chest and Tris',
+      description: 'Lift the bar',
+      reps: 25,
+      weight: 155,
+    });
+
+    expect(action).toEqual({
+      type: SET_NEW_WORKOUT,
+      payload: {
+        name: 'Bench Press',
+        muscles: 'Chest and Tris',
+        description: 'Lift the bar',
+        reps: 25,
+        weight: 155,
+      }
+    });
+  });
 });

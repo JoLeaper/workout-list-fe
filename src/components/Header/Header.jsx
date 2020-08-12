7fde9317ae4c636b6007db77e394a620d4c76bd6
JoLeaper <joleap123@gmail.com>
2020-08-12 13:57:12 -0700

    header component written

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { useCurrentUser } from '../../hooks/authContext';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
+  const AuthLinks = () => (
+    <>
+      <Link to="/signup">Signup</Link>
+      <Link to="/login">Login</Link>
+    </>
+  );
+
+  const currentUser = useCurrentUser();
   return (
-    <div>
-      
-    </div>
+    <>
+      {currentUser 
+        ? <img src={currentUser.profileImage} />
+        : <AuthLinks /> }
+    </>
   );
 }

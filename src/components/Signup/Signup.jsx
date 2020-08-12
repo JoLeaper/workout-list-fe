fd87990fbe4c617523980df2035b897051d42028
JoLeaper <joleap123@gmail.com>
2020-08-12 11:54:52 -0700

    Signup component created

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSignup } from '../../hooks/authContext';
+import { useHistory } from 'react-router-dom';
 
 export default function Signup() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [profileImage, setProfileImage] = useState('');
+  const signup = useSignup();
+  const history = useHistory();
+  
+  const handleChange = ({ target }) => {
+    if(target.name === 'email') setEmail(target.value);
+    if(target.name === 'password') setPassword(target.value);
+    if(target.name === 'profileImage') setProfileImage(target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const signup = useSignup();
+    signup(email, password, profileImage);
+  };
   return (
     <div>
-      
+      <form handleSubmit={handleSubmit}>
+        <input name='email' onChange={handleChange}/>
+        <input name='password' onChange={handleChange}/>
+        <input name='profileImage' onChange={handleChange}/>
+        <button>Sign Up</button>
+      </form>
     </div>
   );
 }
